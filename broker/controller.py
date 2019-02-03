d955c9dd2051d986302b758a676b70f7da29acfd
Richard Imms <richardimms26@gmail.com>
2019-02-03 15:08:48 +0000

    Intialise the broker class

---
+++ broker/controller.py
@@ -0,0 +1,15 @@
+class PositionController():
+
+    def __init__(self):
+        pass
+
+    def openPosition(self):
+        pass
+    
+    def closePosition(self):
+        pass
+    
+    def recieveSignal(self):
+        #Gets the given signal from the broker.
+        #Performs the given tasks - e.g open and close the position.
+        pass
